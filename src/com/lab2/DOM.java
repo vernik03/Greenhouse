5a874d6bc251bdad5f8d9410a19218cd9d51b303
vernik03 <vernik03@gmail.com>
2022-11-14 02:39:50 +0200

    DOM works for class

--- src/com/lab2/DOM.java
+++ src/com/lab2/DOM.java
@@ -1,6 +1,7 @@
 package com.lab2;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -19,10 +20,10 @@ public class DOM {
             DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             // The DOM tree of the document is created from the file
             Document document = documentBuilder.parse(path);
-            Node root = document.getDocumentElement();
-            System.out.println(name);
-            System.out.print(separator);
-            recursionPrint(root, 0);
+//            printFromXML(document.getDocumentElement(), name);
+            ArrayList<Flower> FlowersArray = parseToClass_FlowersArray(document.getDocumentElement());
+            printFromArray(FlowersArray);
+
         } catch (ParserConfigurationException ex) {
             ex.printStackTrace(System.out);
         } catch (SAXException ex) {
@@ -30,6 +31,104 @@ public class DOM {
         } catch (IOException ex) {
             ex.printStackTrace(System.out);
         }
+    }
+
+    public static void printFromArray(ArrayList<Flower> FlowerArray) {
+        for (Flower flower : FlowerArray) {
+            flower.printClass();
+            System.out.println(separator);
+        }
+    }
+
+    private static ArrayList<Flower> parseToClass_FlowersArray(Node root) {
+        ArrayList<Flower> flowers = new ArrayList<>();
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() != Node.TEXT_NODE) {
+                Flower flower = parseToClass_Flower(node);
+                flowers.add(flower);
+            }
+        }
+        return flowers;
+    }
+
+    private static Flower parseToClass_Flower(Node root) {
+        NodeList nodeList = root.getChildNodes();
+        Flower flower = new Flower();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() != Node.TEXT_NODE) {
+                switch (node.getNodeName()) {
+                    case "name":
+                        flower.setName(node.getChildNodes().item(0).getTextContent());
+                        break;
+                    case "soil":
+                        flower.setSoil(node.getChildNodes().item(0).getTextContent());
+                        break;
+                    case "origin":
+                        flower.setOrigin(node.getChildNodes().item(0).getTextContent());
+                        break;
+                    case "visualParameters":
+                        parseToClass_Visual_parameters(node, flower);
+                        break;
+                    case "growingTips":
+                        parseToClass_Growing_tips(node, flower);
+                        break;
+                    case "multiplying":
+                        flower.setMultiplying(node.getChildNodes().item(0).getTextContent());
+                        break;
+                }
+            }
+        }
+
+        return flower;
+    }
+
+    private static void parseToClass_Visual_parameters(Node root, Flower flower) {
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() != Node.TEXT_NODE) {
+                switch (node.getNodeName()) {
+                    case "stem_color":
+                        flower.setStem_color(node.getChildNodes().item(0).getTextContent());
+                        break;
+                    case "leaf_color":
+                        flower.setLeaf_color(node.getChildNodes().item(0).getTextContent());
+                        break;
+                    case "size":
+                        flower.setAverageSize(node.getChildNodes().item(0).getTextContent());
+                        break;
+                }
+            }
+        }
+    }
+
+    private static void parseToClass_Growing_tips(Node root, Flower flower) {
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() != Node.TEXT_NODE) {
+                switch (node.getNodeName()) {
+                    case "temperature":
+                        flower.setTemperature(Float.valueOf(node.getChildNodes().item(0).getTextContent()));
+                        break;
+                    case "is_photophilous":
+                        flower.setLighting(Boolean.valueOf(node.getChildNodes().item(0).getTextContent()));
+                        break;
+                    case "watering":
+                        flower.setWatering(Float.valueOf(node.getChildNodes().item(0).getTextContent()));
+                        break;
+                }
+            }
+        }
+    }
+
+    private static void printFromXML(Node root, String name){
+        System.out.println(name);
+        System.out.print(separator);
+        recursionPrint(root, 0);
     }
 
     private static void recursionPrint(Node root, int level) {
--- src/com/lab2/Flower.java
+++ src/com/lab2/Flower.java
@@ -1,32 +1,56 @@
 package com.lab2;
 
 import java.awt.*;
+import java.security.PublicKey;
 
 public class Flower {
     public enum SoilEnum {
-        PODZOLIC,
-        GROUND,
-        SODDY_PODZOLIC,
+        PODZOLIC("Podzolic soil"),
+        GROUND("Ground soil"),
+        SODDY_PODZOLIC("Soddy-podzolic soil");
+        private String soil;
+        SoilEnum(String soil) {
+            this.soil = soil;}
+        public String getSoil() {
+            return soil;}
     }
 
     public enum MultiplyingEnum {
-        BY_LEAVES,
-        BY_CUTTINGS,
-        BY_SEEDS,
+        BY_LEAVES("Multiplying by leaves"),
+        BY_CUTTINGS("Multiplying by cuttings"),
+        BY_SEEDS("Multiplying by seeds");
+        private String multiplying;
+        MultiplyingEnum(String multiplying) {
+            this.multiplying = multiplying;}
+        public String getMultiplying() {
+            return multiplying;}
     }
 
     public enum SizeEnum {
-        VERY_SMALL,
-        SMALL,
-        MIDDLE,
-        BIG,
-        VERY_BIG,
+        VERY_SMALL("Very small size"),
+        SMALL("Small size"),
+        MEDIUM("Medium size"),
+        BIG("Big size"),
+        VERY_BIG("Very big size");
+        private String size;
+        SizeEnum(String size) {
+            this.size = size;}
+        public String getSize() {
+            return size;}
     }
 
     public class VisualParameters {
-        Color stem_color;
-        Color leaf_color;
+//        Color stem_color;
+        String stem_color;
+//        Color leaf_color;
+        String leaf_color;
         SizeEnum size;
+//        public VisualParameters(Color stem_color, Color leaf_color, SizeEnum size) {
+        public VisualParameters(String stem_color, String leaf_color, SizeEnum size) {
+            this.stem_color = stem_color;
+            this.leaf_color = leaf_color;
+            this.size = size;
+        }
 
     }
 
@@ -34,6 +58,11 @@ public class Flower {
         Float temperature;
         Boolean is_photophilous;
         Float Watering;
+        public GrowingTips(Float temperature, Boolean is_photophilous, Float watering) {
+            this.temperature = temperature;
+            this.is_photophilous = is_photophilous;
+            Watering = watering;
+        }
 
     }
 
@@ -44,6 +73,78 @@ public class Flower {
     private GrowingTips growingTips;
     private MultiplyingEnum multiplying;
 
+    public Flower(){
+        this.name = "No name";
+        this.soil = SoilEnum.PODZOLIC;
+        this.origin = "No origin";
+        this.visualParameters = new VisualParameters("No color", "No color", SizeEnum.VERY_SMALL);
+        this.growingTips = new GrowingTips(0f, false, 0f);
+        this.multiplying = MultiplyingEnum.BY_LEAVES;
+    }
 
+    public Flower(String name, String soil, String origin,
+                  String stem_color, String leaf_color, String size,
+                  Float temperature, Boolean is_photophilous, Float watering,
+                  String multiplying) {
+        this.name = name;
+        this.soil = SoilEnum.valueOf(soil);
+        this.origin = origin;
+        this.visualParameters = new VisualParameters(stem_color, leaf_color, SizeEnum.valueOf(size));
+        this.growingTips = new GrowingTips(temperature, is_photophilous, watering);
+        this.multiplying = MultiplyingEnum.valueOf(multiplying);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSoil(String soil) {
+        this.soil = SoilEnum.valueOf(soil);
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setStem_color(String stem_color) {
+        this.visualParameters.stem_color = stem_color;
+    }
+
+    public void setLeaf_color(String leaf_color) {
+        this.visualParameters.leaf_color = leaf_color;
+    }
+
+    public void setAverageSize(String size) {
+        this.visualParameters.size = SizeEnum.valueOf(size);
+    }
+
+    public void setTemperature(Float temperature) {
+        this.growingTips.temperature = temperature;
+    }
+
+    public void setLighting(Boolean is_photophilous) {
+        this.growingTips.is_photophilous = is_photophilous;
+    }
+
+    public void setWatering(Float watering) {
+        this.growingTips.Watering = watering;
+    }
+
+    public void setMultiplying(String multiplying) {
+        this.multiplying = MultiplyingEnum.valueOf(multiplying);
+    }
+
+    public void printClass(){
+        System.out.println("Name: " + name);
+        System.out.println("Soil: " + soil.getSoil());
+        System.out.println("Origin: " + origin);
+        System.out.println("Stem color: " + visualParameters.stem_color);
+        System.out.println("Leaf color: " + visualParameters.leaf_color);
+        System.out.println("Size: " + visualParameters.size.getSize());
+        System.out.println("Temperature: " + growingTips.temperature);
+        System.out.println("Is photophilous: " + growingTips.is_photophilous);
+        System.out.println("Watering: " + growingTips.Watering);
+        System.out.println("Multiplying: " + multiplying.getMultiplying());
+    }
 }
 
